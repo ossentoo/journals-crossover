e1cea6b1d8a37fbde1eaf63c28580f291d42cf78
Oscar <898371@UK868411>
2017-01-04 09:13:36 +0000

    Added error tests

---
+++ Source/Journals.Web.Tests/Controllers/ErrorControllerTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Mvc;
+using Medico.Web.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Medico.Web.Tests.Controllers
+{
+    [TestClass]
+    public class ErrorControllerTests
+    {
+        [TestMethod]
+        public void RequestLengthExceeded_Returns_ViewBagMessage()
+        {
+            //Arrange
+            var controller = new ErrorController();
+
+            //Act
+            var actionResult = (ViewResult)controller.RequestLengthExceeded();
+            var model = (HandleErrorInfo)actionResult.Model;
+
+            //Assert
+            Assert.AreEqual("Uploading file this large is not supported. Please try again.", model.Exception.Message);
+            Assert.AreEqual("Error", model.ControllerName);
+            Assert.AreEqual("RequestLengthExceeded", model.ActionName);
+        }
+    }
+}
